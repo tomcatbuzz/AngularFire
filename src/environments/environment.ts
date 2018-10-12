158ef7b683672f49f0a394feebaec41529f45f1b
Tony <tomcatbuzz@yahoo.com>
2018-10-12 06:27:16 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD--sXglq0Uve45-tQxAf1sWJueLDMl598',
-    authDomain: 'angularfire-b748a.firebaseapp.com',
-    databaseURL: 'https://angularfire-b748a.firebaseio.com',
-    projectId: 'angularfire-b748a',
-    storageBucket: 'angularfire-b748a.appspot.com',
-    messagingSenderId: '187420711755'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
 
